c7b6c77960ee7330bc508904ea8f2cee3283c9a7
Ruben <rvazquezcaceres@gmail.com>
2020-01-17 14:30:01 +0100

    Starting api register

--- app/src/main/java/com/example/ifoodie/LoginActivity.java
+++ app/src/main/java/com/example/ifoodie/LoginActivity.java
@@ -14,6 +14,8 @@ public class LoginActivity extends AppCompatActivity {
 
     TextView texto;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

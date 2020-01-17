ca83d17c034fe3e006616e23b589c3ff0d32b498
Ruben <rvazquezcaceres@gmail.com>
2020-01-17 14:32:40 +0100

    Start api connection

--- app/src/main/java/com/example/ifoodie/RegisterActivity.java
+++ app/src/main/java/com/example/ifoodie/RegisterActivity.java
@@ -6,13 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText editName;
     EditText editMail;
     EditText editPass;
+    Button accept;
+
+    Retrofit retrofit;
+    BienestarApi bienestarApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +35,26 @@ public class RegisterActivity extends AppCompatActivity {
         editName = findViewById(R.id.editName);
         editMail = findViewById(R.id.editMail);
         editPass = findViewById(R.id.editPass);
+        accept = findViewById(R.id.accept);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:8888/Ruben/Bienestapp/public/index.php/api/") // URL del servidor (API)
+                .addConverterFactory(ScalarsConverterFactory.create()) // Conversor de tipos primitivos
+                .addConverterFactory(GsonConverterFactory.create()) // Conversor de JSON
+                .build();
+        bienestarApi = retrofit.create(BienestarApi.class);
+
+    accept.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            btAccept();
+        }
+    });
+
     }
 
 
-    public void btAccept(View view) {
+    void btAccept() {
         String nombre = editName.getText().toString();
         String mail = editMail.getText().toString();
         String pass = editPass.getText().toString();
@@ -33,11 +62,10 @@ public class RegisterActivity extends AppCompatActivity {
         if (nombre.isEmpty() || mail.isEmpty() || pass.isEmpty()){
             Log.d("Ruben","Error: faltan campos por rellenar");
         } else {
-            Log.d("Ruben","Campos rellenados");
-            Intent intent = new Intent();
-            intent.putExtra("textoEnviado", nombre);
-            setResult(RESULT_OK, intent);
-            finish();
+            Call<String> call = bienestarApi.postRegister(nombre, mail, pass);
+
+            Log.d("Ruben","Error: no funciona");
+
         }
     }
 }
